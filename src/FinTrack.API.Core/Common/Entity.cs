98d0a87857799469cbd20c287c24291335c5e802
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-06-07 15:13:26 +0300

    feat(core): add abstract class Entity

---
+++ src/FinTrack.API.Core/Common/Entity.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace FinTrack.API.Core.Common
+{
+    /// <summary>
+    /// presents abstract class of Entity
+    /// </summary>
+    
+    public abstract class Entity
+    {
+        public Guid Id { get; private set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Entity entity)
+            {
+                return entity.Id == Id;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
