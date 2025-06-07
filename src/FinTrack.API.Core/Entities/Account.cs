3126739c02030d59af22f331b63b5eeb572369af
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-06-07 20:59:18 +0300

    feat(core): add Account entity

---
+++ src/FinTrack.API.Core/Entities/Account.cs
@@ -0,0 +1,43 @@
+﻿
+using FinTrack.API.Core.Common;
+
+namespace FinTrack.API.Core.Entities
+{   
+    /// <summary>
+    /// presents User's account
+    /// </summary>
+    public class Account : Entity
+    {
+        public Account(Guid userId)
+        {
+            UserId = userId;
+        }
+
+        public decimal Balance { get; private set; } = 0;
+        public Guid UserId { get; private set; }
+
+        /// <summary>
+        /// debit money from balance
+        /// </summary>
+        /// <param name="amount">amount that will be debited</param>
+        /// <exception cref="InvalidOperationException">amount param is greater than balance</exception>
+        public void Debit(decimal amount)
+        {
+            if (Balance < amount)
+            {
+                throw new InvalidOperationException("Insufficient funds");
+            }
+            Balance -= amount;
+        }
+
+
+        /// <summary>
+        /// top up the balance
+        /// </summary>
+        /// <param name="amount">amount that will be credited to the balance</param>
+        public void TopUp(decimal amount)
+        {
+            Balance += amount;
+        }
+    }
+}
