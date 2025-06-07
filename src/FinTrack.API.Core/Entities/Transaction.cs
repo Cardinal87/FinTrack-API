a7c4d2a47c4069e6209edc2cf3d54b903ebd9fc1
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-06-07 20:59:43 +0300

    feat(core): add Transaction entity

---
+++ src/FinTrack.API.Core/Entities/Transaction.cs
@@ -0,0 +1,26 @@
+﻿
+using FinTrack.API.Core.Common;
+
+namespace FinTrack.API.Core.Entities
+{
+
+    /// <summary>
+    /// presents p2p transaction
+    /// </summary>
+    public class Transaction : Entity
+    {
+        public Transaction(decimal amount, Guid fromAccountId, Guid toAccountId, DateTime date)
+        {
+            Amount = amount;
+            FromAccountId = fromAccountId;
+            ToAccountId = toAccountId;
+            Date = DateTime.UtcNow;
+        }
+
+        public decimal Amount { get; }
+        public Guid FromAccountId { get;}
+        public Guid ToAccountId { get;}
+        public DateTime Date { get; }
+
+    }
+}
