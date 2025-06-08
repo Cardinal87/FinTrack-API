0fd2805cf900e15ba747aae8773ea103798c5d31
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-06-08 20:04:14 +0300

    feat(core): add validation for User properties

--- src/FinTrack.API.Core/Entities/User.cs
+++ src/FinTrack.API.Core/Entities/User.cs
@@ -1,5 +1,6 @@
 ﻿using FinTrack.API.Core.Common;
-
+using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
 namespace FinTrack.API.Core.Entities
 {
     
@@ -8,6 +9,16 @@ namespace FinTrack.API.Core.Entities
     /// </summary>
     public class User : Entity
     {
+        private const string phonePattern = @"^\+?[1-9][0-9]{7,14}$";
+        private const string emailPattern = @"^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$";
+
+
+        private string name;
+        private string email;
+        private string phone;
+        private string passwordHash;
+        
+        
         public User(string email, string phone, string name, string hash, Guid accountId)
         {
             Email = email;
@@ -17,12 +28,59 @@ namespace FinTrack.API.Core.Entities
             AccountId = accountId;
         }
 
-        public string Email { get; private set; }
-        public string Phone { get; private set; }
-        public string Name { get; private set; }
-        public string PasswordHash { get; private set; }
+        
 
         public Guid AccountId { get; private set; }
-
+        
+        public string Name { 
+            get => name; [MemberNotNull(nameof(name))]
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty or whitespace");
+                }
+                name = value;
+            }
+        }
+        public string Email 
+        { 
+            get => email;
+            [MemberNotNull(nameof(email))]
+            set 
+            {
+                if (!Regex.IsMatch(value, emailPattern))
+                {
+                    throw new ArgumentException("Invalid email");
+                }
+                email = value;
+            } 
+        }
+        public string Phone 
+        { 
+            get => phone;
+            [MemberNotNull(nameof(phone))]
+            set
+            {
+                if (!Regex.IsMatch(value, phonePattern))
+                {
+                    throw new ArgumentException("Invalid phone");
+                }
+                phone = value;
+            }
+        }
+        public string PasswordHash
+        {
+            get => passwordHash;
+            [MemberNotNull(nameof(passwordHash))]
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Password hash cannot be empty or whitespace");
+                }
+                passwordHash = value;
+            }
+        }
     }
 }
