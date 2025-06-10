9563d43c8b47b2664edff412718d6d2614499b25
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-06-10 12:33:50 +0300

    feat(infrasturcture): add EF Core configuration for Account

---
+++ FinTrack/FinTrack.API.Infrastructure/Data/Configurations/AccountConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using FinTrack.API.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FinTrack.API.Infrastructure.Data.Configurations
+{
+    class AccountConfiguration : IEntityTypeConfiguration<Account>
+    {
+        public void Configure(EntityTypeBuilder<Account> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(t => t.Balance).HasPrecision(18,2).IsRequired();
+            builder.Property(t => t.UserId).IsRequired();
+            
+            
+            builder.HasMany(t => t.Transactions)
+                .WithOne()
+                .HasForeignKey(t => t.FromAccountId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .IsRequired(false);
+
+            builder.HasMany(t => t.Transactions)
+                .WithOne()
+                .HasForeignKey(t => t.ToAccountId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .IsRequired(false);
+
+        }
+    }
+}
