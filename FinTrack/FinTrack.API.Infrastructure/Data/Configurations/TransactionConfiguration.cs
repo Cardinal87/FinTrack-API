0ea4e9b261fdcb24b6282400b9ebb47d2c874727
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-06-10 13:07:09 +0300

    feat(infrasturcture): add EF Core configuration for Transaction

---
+++ FinTrack/FinTrack.API.Infrastructure/Data/Configurations/TransactionConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using FinTrack.API.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FinTrack.API.Infrastructure.Data.Configurations
+{
+    class TransactionConfiguration : IEntityTypeConfiguration<Transaction>
+    {
+        public void Configure(EntityTypeBuilder<Transaction> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(t => t.Amount).HasPrecision(18, 2).IsRequired();
+            builder.Property(t => t.FromAccountId).IsRequired();
+            builder.Property(t => t.ToAccountId).IsRequired();
+            builder.Property(t => t.Date).HasColumnType("timestamp");
+        }
+    }
+}
