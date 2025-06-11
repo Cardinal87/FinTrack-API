d723389388d2746ce56e7c8ee53d0471a0334dfc
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-06-11 14:13:02 +0300

    feat(infrastructure): add AccountRepository

---
+++ FinTrack/FinTrack.API.Infrastructure/Data/Repositories/AccountRepository.cs
@@ -0,0 +1,53 @@
+﻿using FinTrack.API.Core.Entities;
+using FinTrack.API.Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinTrack.API.Infrastructure.Data.Repositories
+{
+    class AccountRepository : IAccountRepository
+    {
+        private DatabaseClient _client;
+
+        public AccountRepository(DatabaseClient client)
+        {
+            _client = client;
+        }
+
+        async public Task DeleteAsync(Guid id)
+        {
+            var account = await GetByIdAsync(id);
+            if (account == null)
+            {
+                throw new KeyNotFoundException("Entity with given id does not exist");
+            }
+            _client.Accounts.Remove(account);
+        }
+
+        async public Task<IEnumerable<Account>> GetAllAsync()
+        {
+            return await _client.Accounts.ToListAsync();
+        }
+
+        async public Task<Account?> GetByIdAsync(Guid id)
+        {
+            return await _client.Accounts.FindAsync(id);
+        }
+
+        public void Save(Account account)
+        {
+            _client.Accounts.Add(account);
+        }
+
+        async public Task UpdateAsync(Account account)
+        {
+            var existingAccount = await GetByIdAsync(account.Id);
+            if (existingAccount == null)
+            {
+                throw new KeyNotFoundException("Entity with given id does not exist");
+            }
+            _client.Accounts.Update(account);
+        }
+
+        public async Task SaveChangesAsync() => await _client.SaveChangesAsync();
+    }
+}
