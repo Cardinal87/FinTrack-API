983c338e72ad344512f371b55fe047b6cb8ebd69
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-06-13 12:53:14 +0300

    feat(application): add use case for deleting user

---
+++ FinTrack/FinTrack.API.Application/UseCases/Users/DeleteUser/DeleteUserCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace FinTrack.API.Application.UseCases.Users.DeleteUser
+{
+    /// <summary>
+    /// Represents MediatR command for deleting existing user
+    /// </summary>
+    /// <param name="id">id of the existing user to delete</param>
+    public record DeleteUserCommand(Guid id) : IRequest;
+   
+}
---
+++ FinTrack/FinTrack.API.Application/UseCases/Users/DeleteUser/DeleteUserHandler.cs
@@ -0,0 +1,36 @@
+﻿
+using FinTrack.API.Core.Interfaces;
+using MediatR;
+
+namespace FinTrack.API.Application.UseCases.Users.DeleteUser
+{
+    /// <summary>
+    /// Handles user deleting
+    /// </summary>
+    /// <remarks>
+    /// Steps:
+    /// <para>1. Delete user with given id</para>
+    /// <para>2. Persist data via repository</para>
+    /// 
+    /// <para>
+    /// Exceptions:
+    /// <para>- <see cref="KeyNotFoundException"/>: User with given id does not exist</para>
+    /// </para>
+    /// </remarks>
+    internal class DeleteUserHandler : IRequestHandler<DeleteUserCommand>
+    {
+
+        private IUserRepository _userRepository;
+
+        public DeleteUserHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        async public Task Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+        {
+            await _userRepository.DeleteAsync(request.id);
+            await _userRepository.SaveChangesAsync();
+        }
+    }
+}
