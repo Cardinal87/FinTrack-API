aaaae9e5012910fc3f7d2ccd6df524f26dd416bf
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-06-14 18:59:37 +0300

    feat(core): add InvalidTransactionException

---
+++ FinTrack/FinTrack.API.Core/Exceptions/InvalidTransactionException.cs
@@ -0,0 +1,28 @@
+﻿using FinTrack.API.Core.Common;
+
+namespace FinTrack.API.Core.Exceptions
+{
+    /// <summary>
+    /// Exception for transactions
+    /// </summary>
+    /// <remarks>
+    /// Must be thrown in case of incorrect transaction handling
+    /// with indicating the reason
+    /// </remarks>
+    public sealed class InvalidTransactionException : DomainException
+    {
+        /// <summary>
+        /// Short description of the reason of the exception
+        /// </summary>
+        public string Reason => (string)Data["Reason"]!;
+        
+        /// <summary>
+        /// </summary>
+        /// <param name="reason">reason of the exception</param>
+        public InvalidTransactionException(string reason) 
+            : base($"Transaction invalid: {reason}")
+        {
+            Data.Add("Reason", reason);
+        }
+    }
+}
