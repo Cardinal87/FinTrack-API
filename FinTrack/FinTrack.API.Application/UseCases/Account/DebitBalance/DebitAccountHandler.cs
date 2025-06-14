a68f90515adf6ca743fa2b0a55374aeac0511bc9
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-06-14 12:13:02 +0300

    feat(application): add use case to debit balance

---
+++ FinTrack/FinTrack.API.Application/UseCases/Account/DebitBalance/DebitAccountHandler.cs
@@ -0,0 +1,48 @@
+﻿using FinTrack.API.Core.Interfaces;
+using MediatR;
+
+namespace FinTrack.API.Application.UseCases.Account.DebitBalance
+{
+
+    /// <summary>
+    /// Handles balance debit
+    /// </summary>
+    /// <remarks>
+    /// Steps:
+    /// <para>1. Check if the account with given id exists</para>
+    /// <para>2. Use entity method to debit balance</para>
+    /// <para>3. Use repository to update entity and save changes</para>
+    /// 
+    /// <para>
+    /// Exceptions:
+    /// <para> - <see cref="ArgumentException"/>: debit amount is negative or equal to zero</para>
+    /// <para> - <see cref="ArgumentNullException"/>: account with given id does not exists</para>
+    /// <para> - <see cref="InvalidOperationException"/>: debit amount is greater than balance of the account</para>
+    /// 
+    /// </para>
+    /// </remarks>                                               
+    internal class DebitAccountHandler : IRequestHandler<DebitBalanceCommand, decimal>
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public DebitAccountHandler(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        async public Task<decimal> Handle(DebitBalanceCommand request, CancellationToken cancellationToken)
+        {
+            var account = await _accountRepository.GetByIdAsync(request.accountId);
+            if (account == null)
+            {
+                throw new ArgumentNullException("account with given id does not exist");
+            }
+
+            account.Debit(request.amount);
+            await _accountRepository.UpdateAsync(account);
+            await _accountRepository.SaveChangesAsync();
+
+            return account.Balance;
+        }
+    }
+}
---
+++ FinTrack/FinTrack.API.Application/UseCases/Account/DebitBalance/DebitBalanceCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace FinTrack.API.Application.UseCases.Account.DebitBalance
+{
+    /// <summary>
+    /// Represents MediatR command to debit balance.
+    /// Returns balance of the account
+    /// </summary>
+    /// <param name="accountId">Source account id</param>
+    /// <param name="amount">
+    /// Debit amount.
+    /// Must be positive
+    /// </param>
+    public record DebitBalanceCommand(Guid accountId, decimal amount) : IRequest<decimal>;
+    
+}
