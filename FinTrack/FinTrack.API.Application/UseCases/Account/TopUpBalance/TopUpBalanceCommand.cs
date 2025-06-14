863acc67de7a7dd59285247b3f08b56785317daa
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-06-14 11:52:30 +0300

    feat(application): add use case to top up balance

---
+++ FinTrack/FinTrack.API.Application/UseCases/Account/TopUpBalance/TopUpBalanceCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+namespace FinTrack.API.Application.UseCases.Account.TopUpBalance
+{
+    /// <summary>
+    /// Represents MediatR command to top up balance.
+    /// Returns balance of the account
+    /// </summary>
+    /// <param name="accountId">Destination account id</param>
+    /// <param name="amount">
+    /// Deposit amount.
+    /// Must be positive
+    /// </param>
+    public record TopUpBalanceCommand(Guid accountId, decimal amount) : IRequest<decimal>;
+    
+}
---
+++ FinTrack/FinTrack.API.Application/UseCases/Account/TopUpBalance/TopUpBalanceHandler.cs
@@ -0,0 +1,47 @@
+﻿
+using FinTrack.API.Core.Interfaces;
+using MediatR;
+
+namespace FinTrack.API.Application.UseCases.Account.TopUpBalance
+{
+
+    /// <summary>
+    /// Handles account deposits
+    /// </summary>
+    /// <remarks>
+    /// Steps:
+    /// <para>1. Check if account with given id exists</para>
+    /// <para>2. Top up balance using entity method</para>
+    /// <para>3. Update entity in repository and save changes</para>
+    /// 
+    /// <para>
+    /// Exceptions:
+    /// <para> - <see cref="ArgumentException"/>: amount of the deposit is negative or equal to zero</para>
+    /// <para> - <see cref="ArgumentNullException"/>: account does not exist</para>
+    /// </para>
+    /// </remarks>
+    internal class TopUpBalanceHandler : IRequestHandler<TopUpBalanceCommand, decimal>
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public TopUpBalanceHandler(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        async public Task<decimal> Handle(TopUpBalanceCommand request, CancellationToken cancellationToken)
+        {
+            var account = await _accountRepository.GetByIdAsync(request.accountId);
+            if (account == null)
+            {
+                throw new KeyNotFoundException("Account with given id does not exist");
+            }
+            account.TopUp(request.amount);
+
+            
+            await _accountRepository.UpdateAsync(account);
+            await _accountRepository.SaveChangesAsync();
+            return account.Balance;
+        }
+    }
+}
