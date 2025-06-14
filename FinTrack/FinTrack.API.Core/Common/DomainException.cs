39bfe078c6894896f5d4d43dc13c113202c3805f
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-06-14 18:57:20 +0300

    feat(core): add base class for domain exceptions

---
+++ FinTrack/FinTrack.API.Core/Common/DomainException.cs
@@ -0,0 +1,12 @@
+﻿
+namespace FinTrack.API.Core.Common
+{
+    
+    /// <summary>
+    /// Represents base class for domain layer exceptions
+    /// </summary>
+    public class DomainException : Exception
+    {
+        public DomainException(string message) : base(message) { }
+    }
+}
