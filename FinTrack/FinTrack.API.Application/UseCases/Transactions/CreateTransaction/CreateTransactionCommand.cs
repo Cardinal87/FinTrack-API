62d2758474665bb985785746e4efa81c436ac718
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-06-14 11:45:33 +0300

    feat(application): add use case for creating transactions

---
+++ FinTrack/FinTrack.API.Application/UseCases/Transactions/CreateTransaction/CreateTransactionCommand.cs
@@ -0,0 +1,25 @@
+﻿
+
+using MediatR;
+
+namespace FinTrack.API.Application.UseCases.Transactions.CreateTransaction
+{
+    /// <summary>
+    /// Represents MediatR command for creating transaction.
+    /// Returns Guid of created transaction
+    /// </summary>
+    /// <param name="amount">
+    /// Amount of the transaction.
+    /// Must be positive
+    /// </param>
+    /// <param name="fromAccountId">
+    /// Source account id.
+    /// Must not match with <paramref name="toAccountId"/> 
+    /// </param>
+    /// <param name="toAccountId">
+    /// Destination account id.
+    /// Must not match with <paramref name="fromAccountId"/>
+    /// </param>
+    public record CreateTransactionCommand(decimal amount, Guid fromAccountId, Guid toAccountId) : IRequest<Guid>;
+    
+}
---
+++ FinTrack/FinTrack.API.Application/UseCases/Transactions/CreateTransaction/CreateTransactionHandler.cs
@@ -0,0 +1,30 @@
+﻿using FinTrack.API.Core.Services;
+using MediatR;
+namespace FinTrack.API.Application.UseCases.Transactions.CreateTransaction
+{
+    
+    /// <summary>
+    /// Handles trasaction between accounts
+    /// </summary>
+    /// <remarks>
+    /// Use core service for handle transaction
+    /// </remarks>
+    internal class CreateTransactionHandler : IRequestHandler<CreateTransactionCommand, Guid>
+    {
+        private TransferService _transferService;
+
+        public CreateTransactionHandler(TransferService transferService)
+        {
+            _transferService = transferService;
+        }
+
+        async public Task<Guid> Handle(CreateTransactionCommand request, CancellationToken cancellationToken)
+        {
+            var guid = await _transferService.HandleTransactionAsync(request.amount,
+                                                          request.toAccountId,
+                                                          request.fromAccountId);
+
+            return guid;
+        }
+    }
+}
