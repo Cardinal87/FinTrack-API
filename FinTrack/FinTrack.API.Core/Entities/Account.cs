6540c255b536d653d4197e9081378e1c22ef88a0
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-06-14 11:44:12 +0300

    fix(core): update Account invariant protection

--- FinTrack/FinTrack.API.Core/Entities/Account.cs
+++ FinTrack/FinTrack.API.Core/Entities/Account.cs
@@ -1,6 +1,5 @@
 ﻿
 using FinTrack.API.Core.Common;
-
 namespace FinTrack.API.Core.Entities
 {   
     /// <summary>
@@ -8,20 +7,23 @@ namespace FinTrack.API.Core.Entities
     /// </summary>
     public class Account : Entity
     {
+        private readonly List<Transaction> _outgoingTransactions = new();
+        private readonly List<Transaction> _incomingTransactions = new();
+        
         private Account()
         {
 
         }
         
-        public Account(Guid userId)
+        internal Account(Guid userId)
         {
             UserId = userId;
         }
 
         public decimal Balance { get; private set; } = 0;
         public Guid UserId { get; private set; }
-        public ICollection<Transaction> OutgoingTransactions { get; private set; } = new List<Transaction>();
-        public ICollection<Transaction> IncomingTransactions { get; private set; } = new List<Transaction>();
+        public IReadOnlyCollection<Transaction> OutgoingTransactions => _outgoingTransactions.AsReadOnly();
+        public IReadOnlyCollection<Transaction> IncomingTransactions => _incomingTransactions.AsReadOnly();
 
         /// <summary>
         /// debit money from balance
@@ -31,8 +33,12 @@ namespace FinTrack.API.Core.Entities
         public void Debit(decimal amount)
         {
             if (Balance < amount)
-            {
+            {  
                 throw new InvalidOperationException("Insufficient funds");
+            }
+            if (amount <= 0)
+            {
+                throw new ArgumentException("amount must be positive");
             }
             Balance -= amount;
         }
@@ -44,7 +50,30 @@ namespace FinTrack.API.Core.Entities
         /// <param name="amount">amount that will be credited to the balance</param>
         public void TopUp(decimal amount)
         {
+            if (amount <= 0)
+            {
+                throw new ArgumentException("amount must be positive");
+            }
             Balance += amount;
+        }
+
+        internal void AddOutgoingTransaction(Transaction transaction)
+        {
+            if (transaction.FromAccountId != Id)
+            {
+                throw new ArgumentException("Transaction not for this account");
+            }
+            _outgoingTransactions.Add(transaction);
+        }
+
+        internal void AddIncomingTransaction(Transaction transaction)
+        {
+            if (transaction.ToAccountId != Id)
+            {
+                throw new ArgumentException("Transaction not for this account");
+            }
+            _incomingTransactions.Add(transaction);
         }
     }
 }
+
