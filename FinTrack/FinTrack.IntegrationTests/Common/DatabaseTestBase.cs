693f3365fdf3ebc95ee3a2af7e53461516ec6d96
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-06-16 21:23:06 +0300

    test(infrastructure): add base class for database tests

---
+++ FinTrack/FinTrack.IntegrationTests/Common/DatabaseTestBase.cs
@@ -0,0 +1,36 @@
+﻿
+using FinTrack.API.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.InteropServices.Marshalling;
+using Testcontainers.PostgreSql;
+
+namespace FinTrack.IntegrationTests.Common
+{
+    public class DatabaseTestBase : IAsyncLifetime
+    {
+        protected DatabaseClient _client = null!;
+        private readonly PostgreSqlContainer _container = new PostgreSqlBuilder()
+            .WithDatabase("test_db")
+            .WithUsername("test_user")
+            .WithPassword("test_password")
+            .WithCleanUp(true)
+            .Build();
+        async public Task DisposeAsync()
+        {
+            await _container.DisposeAsync();
+        }
+
+        virtual async public Task InitializeAsync()
+        {
+            await _container.StartAsync();
+
+            var options = new DbContextOptionsBuilder<DatabaseClient>()
+                .UseNpgsql(_container.GetConnectionString())
+                .Options;
+
+            _client = new DatabaseClient(options);
+            await _client.Database.MigrateAsync();
+
+        }
+    }
+}
