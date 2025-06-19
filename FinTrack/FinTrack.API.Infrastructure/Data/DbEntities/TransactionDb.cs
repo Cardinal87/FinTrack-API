61e0eb820b4d0199912fcbd7781095d8c62fa487
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-06-19 12:19:50 +0300

    feat(infrastructure): add TransactionDb entity

---
+++ FinTrack/FinTrack.API.Infrastructure/Data/DbEntities/TransactionDb.cs
@@ -0,0 +1,13 @@
+﻿
+namespace FinTrack.API.Infrastructure.Data.DbEntities
+{
+    public class TransactionDb
+    {
+        public TransactionDb() { }
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public Guid FromAccountId { get; set; }
+        public Guid ToAccountId { get; set; }
+    }
+}
