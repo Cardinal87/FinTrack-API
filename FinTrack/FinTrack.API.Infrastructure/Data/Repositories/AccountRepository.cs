117a87acef173facf1f596172850224dec87f82a
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-06-20 18:17:06 +0300

    refactor(infrastructure): include mapper to AccountRepository

--- FinTrack/FinTrack.API.Infrastructure/Data/Repositories/AccountRepository.cs
+++ FinTrack/FinTrack.API.Infrastructure/Data/Repositories/AccountRepository.cs
@@ -1,5 +1,7 @@
-﻿using FinTrack.API.Core.Entities;
+﻿using AutoMapper;
+using FinTrack.API.Core.Entities;
 using FinTrack.API.Core.Interfaces;
+using FinTrack.API.Infrastructure.Data.DbEntities;
 using Microsoft.EntityFrameworkCore;
 
 namespace FinTrack.API.Infrastructure.Data.Repositories
@@ -7,15 +9,17 @@ namespace FinTrack.API.Infrastructure.Data.Repositories
     public class AccountRepository : IAccountRepository
     {
         private DatabaseClient _client;
+        private IMapper _mapper;
 
-        public AccountRepository(DatabaseClient client)
+        public AccountRepository(DatabaseClient client, IMapper mapper)
         {
             _client = client;
+            _mapper = mapper;
         }
 
         async public Task DeleteAsync(Guid id)
         {
-            var account = await GetByIdAsync(id);
+            var account = await _client.Accounts.FindAsync(id);
             if (account == null)
             {
                 throw new KeyNotFoundException("Entity with given id does not exist");
@@ -25,27 +29,32 @@ namespace FinTrack.API.Infrastructure.Data.Repositories
 
         async public Task<IEnumerable<Account>> GetAllAsync()
         {
-            return await _client.Accounts.ToListAsync();
+            var dbList = await _client.Accounts.ToListAsync();
+            var accountList = _mapper.Map<List<Account>>(dbList);
+            return accountList;
         }
 
         async public Task<Account?> GetByIdAsync(Guid id)
         {
-            return await _client.Accounts.FindAsync(id);
+            var dbAccount = await _client.Accounts.FindAsync(id);
+            var account = _mapper.Map<Account>(dbAccount);
+            return account;
         }
 
         public void Add(Account account)
         {
-            _client.Accounts.Add(account);
+            var dbAccount = _mapper.Map<AccountDb>(account);
+            _client.Accounts.Add(dbAccount);
         }
 
         async public Task UpdateAsync(Account account)
         {
-            var existingAccount = await GetByIdAsync(account.Id);
+            var existingAccount = await _client.Accounts.FindAsync(account.Id);
             if (existingAccount == null)
             {
                 throw new KeyNotFoundException("Entity with given id does not exist");
             }
-            _client.Accounts.Update(account);
+            _mapper.Map(account, existingAccount);
         }
 
         public async Task SaveChangesAsync() => await _client.SaveChangesAsync();
