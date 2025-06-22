13f5dcb99c88875274c526e9895ab557230f593e
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-06-22 11:08:53 +0300

    feat(application): add use case for creating accont

---
+++ FinTrack/FinTrack.API.Application/UseCases/Account/CreateAccount/CreateAccountCommand.cs
@@ -0,0 +1,14 @@
+﻿
+using MediatR;
+
+namespace FinTrack.API.Application.UseCases.Account.CreateAccount
+{
+    /// <summary>
+    /// Represents MediatR command to create account.
+    /// Returns id of the created account
+    /// </summary>
+    /// <param name="userId">
+    /// the ID of the user for whom the account will be created
+    /// </param>
+    public record CreateAccountCommand(Guid userId) : IRequest<Guid>;
+}
---
+++ FinTrack/FinTrack.API.Application/UseCases/Account/CreateAccount/CreateAccountHandler.cs
@@ -0,0 +1,35 @@
+﻿
+using FinTrack.API.Core.Interfaces;
+using MediatR;
+
+namespace FinTrack.API.Application.UseCases.Account.CreateAccount
+{
+    /// <summary>
+    /// Handles account creating 
+    /// </summary>
+    /// <remarks>
+    /// Steps:
+    /// <para>1. Create account with given user id</para>
+    /// <para>2. Add account to repository and save changes</para>
+    /// <para>3. Return id of the created account</para>
+    /// 
+    /// </remarks>
+    internal class CreateAccountHandler : IRequestHandler<CreateAccountCommand, Guid>
+    {
+        private readonly IAccountRepository _accountRepository;
+        
+        public CreateAccountHandler(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+        
+        public async Task<Guid> Handle(CreateAccountCommand request, CancellationToken cancellationToken)
+        {
+            var account = new Core.Entities.Account(request.userId);
+            _accountRepository.Add(account);
+            await _accountRepository.SaveChangesAsync();
+
+            return account.Id;
+        }
+    }
+}
