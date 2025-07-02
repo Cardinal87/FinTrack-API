7b19de85a68e1499001b7e48ae6fab86af4aadb6
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-07-02 20:35:52 +0300

    docs(core): update Transaction documentation

--- FinTrack/FinTrack.API.Core/Entities/Transaction.cs
+++ FinTrack/FinTrack.API.Core/Entities/Transaction.cs
@@ -6,10 +6,33 @@ namespace FinTrack.API.Core.Entities
 {
 
     /// <summary>
-    /// presents p2p transaction
+    /// Represents peer-to-peer financial transaction between two accounts.
     /// </summary>
+    /// <remarks>
+    /// Responsibilities:
+    /// <list type="bullet">
+    /// <item>Stores all transaction info</item>
+    /// <item>Emmutable</item>
+    /// </list>
+    /// transaction must be immutable
+    /// </remarks>
     public class Transaction : Entity
     {
+        /// <summary>
+        /// Creates immutable transaction
+        /// </summary>
+        /// <param name="amount">amount of the transaction</param>
+        /// <param name="fromAccountId">source account id</param>
+        /// <param name="toAccountId">destination account id</param>
+        /// <param name="time">date of the transaction</param>
+        /// <exception cref="InvalidTransactionException">
+        /// Thrown when:
+        /// <list type="bullet">
+        /// <item><paramref name="amount"/>is negative or zero</item>
+        /// <item><paramref name="time"/> is later than <see cref="DateTime.UtcNow"/></item>
+        /// <item><paramref name="fromAccountId"/>is equal to <paramref name="toAccountId"/></item>
+        /// </list>
+        /// </exception>
         public Transaction(decimal amount, Guid fromAccountId, Guid toAccountId, DateTime time)
         {
             if (amount <= 0)
@@ -24,16 +47,36 @@ namespace FinTrack.API.Core.Entities
             {
                 throw new InvalidTransactionException("Incorrect date of transaction");
             }
-
             Amount = amount;
             FromAccountId = fromAccountId;
             ToAccountId = toAccountId;
             Date = time;
         }
 
+        /// <summary>
+        /// Amount of the transaction
+        /// </summary>
+        /// <remarks>
+        /// Must be positive and must not be equal to zero 
+        /// </remarks>
         public decimal Amount { get; }
+
+        /// <summary>
+        /// Source account ID
+        /// </summary>
         public Guid FromAccountId { get;}
+
+        /// <summary>
+        /// Destination account ID
+        /// </summary>
         public Guid ToAccountId { get;}
+
+        /// <summary>
+        /// Transaction timestamp in UTC
+        /// </summary>
+        /// <remarks>
+        /// Must be less or equal to <see cref="DateTime.UtcNow"/>
+        /// </remarks>
         public DateTime Date { get; }
 
     }
