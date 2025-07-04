cc717bb984222f4943e2eb56123043d71ce13804
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-07-04 19:54:51 +0300

    feat(application): add interface for jwt token service

---
+++ FinTrack/FinTrack.API.Application/Interfaces/IJwtTokenService.cs
@@ -0,0 +1,18 @@
+﻿
+using FinTrack.API.Core.Entities;
+
+namespace FinTrack.API.Application.Interfaces
+{
+    /// <summary>
+    /// Represents service for generating JWT tokens
+    /// </summary>
+    public interface IJwtTokenService
+    {
+        /// <summary>
+        /// Generates JWT token based on user data
+        /// </summary>
+        /// <param name="user">User domain model</param>
+        /// <returns>JWT token in base64 format</returns>
+        public string GenerateToken(User user);
+    }
+}
