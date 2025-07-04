4c9d87db3b1e388bd23da8d4fd73b995eab7ee99
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-07-04 20:39:06 +0300

    feat(infrastructure): add jwt key service

---
+++ FinTrack/FinTrack.API.Infrastructure/Identity/JwtKeyService.cs
@@ -0,0 +1,39 @@
+﻿
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography;
+
+namespace FinTrack.API.Infrastructure.Identity
+{
+    public class JwtKeyService
+    {
+        private readonly object _lock = new object();
+        private byte[] _key;
+
+        public JwtKeyService()
+        {
+            GenerateKey();
+        }
+
+
+        [MemberNotNull(nameof(_key))]
+        public void GenerateKey()
+        {
+            lock (_lock)
+            {
+                var key = new byte[64];
+                var rng = RandomNumberGenerator.Create();
+                rng.GetBytes(key);
+                _key = key;
+            }
+        }
+
+        public byte[] GetKey()
+        {
+            lock (_lock)
+            {
+                return _key;
+            }
+        } 
+
+    }
+}
