9b0ae2e23ae97c744cc0acdb5ed809eb439a089f
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-07-07 13:05:45 +0300

    feat(identity): add JwtOptions DTO

---
+++ FinTrack/FinTrack.API.Infrastructure/Identity/DTO/JwtOptions.cs
@@ -0,0 +1,11 @@
+﻿
+namespace FinTrack.API.Infrastructure.Identity.DTO
+{
+    public class JwtOptions
+    {
+
+        public string Issuer { get; set; } = null!;
+        public string Audience { get; set; } = null!;
+        public TimeSpan LifeTime { get; set; }
+    }
+}
