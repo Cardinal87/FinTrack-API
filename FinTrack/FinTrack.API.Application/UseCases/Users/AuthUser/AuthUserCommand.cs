39b4221ca441e013896637a1e2f732ec2d6f12b5
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-07-22 20:55:03 +0300

    feat(application): add use case for user authentication

---
+++ FinTrack/FinTrack.API.Application/UseCases/Users/AuthUser/AuthUserCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+namespace FinTrack.API.Application.UseCases.Users.CheckUser
+{
+    /// <summary>
+    /// Represents MediatR command for checking if the user exists
+    /// and are the credentials correct.
+    /// Returns <see langword="true"/> if the user exists and credentials are correct
+    /// else returns <see langword="false"/>.
+    /// </summary>
+    /// <param name="login">User's login</param>
+    /// <param name="password">User's password</param>
+    public record AuthUserCommand(string login,
+                                   string password) : IRequest<bool>;
+}
---
+++ FinTrack/FinTrack.API.Application/UseCases/Users/AuthUser/AuthUserHandler.cs
@@ -0,0 +1,30 @@
+﻿
+
+using FinTrack.API.Application.UseCases.Users.CheckUser;
+using FinTrack.API.Core.Interfaces;
+using MediatR;
+
+namespace FinTrack.API.Application.UseCases.Users.AuthUser
+{
+    internal class AuthUserHandler : IRequestHandler<AuthUserCommand, bool>
+    {
+        private IUserRepository _userRepository;
+        private IPasswordHasher _passwordHasher;
+        
+        public AuthUserHandler(IUserRepository userRepository, IPasswordHasher passwordHasher)
+        {
+            _userRepository = userRepository;
+            _passwordHasher = passwordHasher;
+        }
+
+        async public Task<bool> Handle(AuthUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByEmailAsync(request.login);
+            if (user == null)
+            {
+                return false;
+            }
+            return _passwordHasher.VerifyPassword(user.PasswordHash, request.password);
+        }
+    }
+}
