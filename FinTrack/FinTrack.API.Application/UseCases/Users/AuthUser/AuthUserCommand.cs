8a7cf4ed7456f2befe98eba7aa6209387e96b719
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-07-23 14:31:30 +0300

    feat(application): update AuthUser use case

--- FinTrack/FinTrack.API.Application/UseCases/Users/AuthUser/AuthUserCommand.cs
+++ FinTrack/FinTrack.API.Application/UseCases/Users/AuthUser/AuthUserCommand.cs
@@ -1,14 +1,15 @@
-﻿using MediatR;
-namespace FinTrack.API.Application.UseCases.Users.CheckUser
+﻿using FinTrack.API.Core.Entities;
+using MediatR;
+namespace FinTrack.API.Application.UseCases.Users.AuthUser
 {
     /// <summary>
     /// Represents MediatR command for checking if the user exists
     /// and are the credentials correct.
-    /// Returns <see langword="true"/> if the user exists and credentials are correct
-    /// else returns <see langword="false"/>.
+    /// Returns <see cref="User"/> if exists and credentials are correct
+    /// else returns <see langword="null"/>.
     /// </summary>
     /// <param name="login">User's login</param>
     /// <param name="password">User's password</param>
     public record AuthUserCommand(string login,
-                                   string password) : IRequest<bool>;
+                                   string password) : IRequest<User?>;
 }
--- FinTrack/FinTrack.API.Application/UseCases/Users/AuthUser/AuthUserHandler.cs
+++ FinTrack/FinTrack.API.Application/UseCases/Users/AuthUser/AuthUserHandler.cs
@@ -1,12 +1,12 @@
 ﻿
 
-using FinTrack.API.Application.UseCases.Users.CheckUser;
+using FinTrack.API.Core.Entities;
 using FinTrack.API.Core.Interfaces;
 using MediatR;
 
 namespace FinTrack.API.Application.UseCases.Users.AuthUser
 {
-    internal class AuthUserHandler : IRequestHandler<AuthUserCommand, bool>
+    internal class AuthUserHandler : IRequestHandler<AuthUserCommand, User?>
     {
         private IUserRepository _userRepository;
         private IPasswordHasher _passwordHasher;
@@ -17,14 +17,15 @@ namespace FinTrack.API.Application.UseCases.Users.AuthUser
             _passwordHasher = passwordHasher;
         }
 
-        async public Task<bool> Handle(AuthUserCommand request, CancellationToken cancellationToken)
+        async public Task<User?> Handle(AuthUserCommand request, CancellationToken cancellationToken)
         {
             var user = await _userRepository.GetByEmailAsync(request.login);
-            if (user == null)
+            if (user != null)
             {
-                return false;
+                var isValidCredentials = _passwordHasher.VerifyPassword(user.PasswordHash, request.password);
+                return isValidCredentials? user :  null;
             }
-            return _passwordHasher.VerifyPassword(user.PasswordHash, request.password);
+            return null;
         }
     }
 }
