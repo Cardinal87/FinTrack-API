d849b6d4491c77455b9338da68c6f0f7e35aa500
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-07-23 14:31:47 +0300

    feat(api): add controller for authorization

---
+++ FinTrack/FinTrack.API/Controllers/TokenController.cs
@@ -0,0 +1,44 @@
+﻿using FinTrack.API.Application.Interfaces;
+using FinTrack.API.Application.UseCases.Users.AuthUser;
+using FinTrack.API.DTO;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinTrack.API.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly IJwtTokenService _jwtTokenService;
+
+        public TokenController(IMediator mediator, IJwtTokenService jwtTokenService)
+        {
+            _mediator = mediator;
+            _jwtTokenService = jwtTokenService;
+        }
+
+        [HttpPost("token")]
+        async public Task<IActionResult> GetJwtToken([FromBody] LoginRequest loginRequest)
+        {
+            var request = new AuthUserCommand(loginRequest.Login, loginRequest.Password);
+            var authUser = await _mediator.Send(request);
+            if (authUser == null)
+            {
+                return Unauthorized(new { message = "invalid credentials" });
+            }
+            var token = _jwtTokenService.GenerateToken(authUser, new List<string> { "User" });
+            return Ok(new {token});
+        }
+
+
+        [Authorize]
+        [HttpGet("token/status")]
+        public IActionResult GetJwtStatus()
+        {
+            return Ok(new { status = "token is valid" });
+        }
+    }
+}
---
+++ FinTrack/FinTrack.API/DTO/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace FinTrack.API.DTO
+{
+    public class LoginRequest
+    {
+        public string Password { get; set; } = null!;
+        public string Login { get; set; } = null!;
+    }
+}
