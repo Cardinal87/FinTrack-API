40e78166547f063a1906a05683eca92c6064d39a
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-07-24 18:49:46 +0300

    feat(application): add use case for getting user by id

---
+++ FinTrack/FinTrack.API.Application/UseCases/Users/GetUser/GetUserCommand.cs
@@ -0,0 +1,15 @@
+﻿
+using FinTrack.API.Core.Entities;
+using MediatR;
+
+namespace FinTrack.API.Application.UseCases.Users.GetUser
+{
+    /// <summary>
+    /// Represents MediatR command for getting user by id.
+    /// Returns <see cref="User"/> if exists
+    /// else returns <see langword="null"/>.
+    /// </summary>
+    /// <param name="guid">User id</param>
+    public record GetUserCommand(Guid guid) : IRequest<User?>;
+
+}
---
+++ FinTrack/FinTrack.API.Application/UseCases/Users/GetUser/GetUserHandler.cs
@@ -0,0 +1,23 @@
+﻿using FinTrack.API.Core.Entities;
+using FinTrack.API.Core.Interfaces;
+using MediatR;
+
+
+namespace FinTrack.API.Application.UseCases.Users.GetUser
+{
+    internal class GetUserHandler : IRequestHandler<GetUserCommand, User?>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public GetUserHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        async public Task<User?> Handle(GetUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByIdAsync(request.guid);
+            return user;
+        }
+    }
+}
