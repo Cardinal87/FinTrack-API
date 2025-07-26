ef8727dc03b64ba5bdf985951aea7087024b6348
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-07-26 21:56:27 +0300

    feat(application): add use case for deleting user account

---
+++ FinTrack/FinTrack.API.Application/UseCases/Accounts/DeleteUserAccount/DeleteUserAccountCommand.cs
@@ -0,0 +1,16 @@
+﻿
+using MediatR;
+
+namespace FinTrack.API.Application.UseCases.Accounts.DeleteUserAccount
+{
+    /// <summary>
+    /// Represents MediatR command for deleting user's account.
+    /// </summary>
+    /// <param name="userId">
+    /// Id of the existing user
+    /// </param>
+    /// <param name="accountId">
+    /// Id of the user's account
+    /// </param>
+    public record DeleteUserAccountCommand(Guid userId, Guid accountId) : IRequest;
+}
---
+++ FinTrack/FinTrack.API.Application/UseCases/Accounts/DeleteUserAccount/DeleteUserAccountHandler.cs
@@ -0,0 +1,31 @@
+﻿
+using FinTrack.API.Core.Interfaces;
+using MediatR;
+
+namespace FinTrack.API.Application.UseCases.Accounts.DeleteUserAccount
+{
+    internal class DeleteUserAccountHandler : IRequestHandler<DeleteUserAccountCommand>
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public DeleteUserAccountHandler(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        async public Task Handle(DeleteUserAccountCommand request, CancellationToken cancellationToken)
+        {
+            var account = await _accountRepository.GetByIdAsync(request.accountId);
+            if (account == null)
+            {
+                throw new KeyNotFoundException($"account with id {request.accountId} does not exits");
+            }
+            if (account.UserId != request.userId)
+            {
+                throw new ArgumentException($"account with id {request.accountId} does not belong to user {request.userId}");
+            }
+            await _accountRepository.DeleteAsync(request.accountId);
+            await _accountRepository.SaveChangesAsync();
+        }
+    }
+}
