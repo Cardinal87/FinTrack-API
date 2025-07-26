910580dbac9bf87f04e777555b1397ea4997a659
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-07-26 21:13:46 +0300

    feat(application): add use case for getting account by id

---
+++ FinTrack/FinTrack.API.Application/UseCases/Accounts/GetAccount/GetAccountCommand.cs
@@ -0,0 +1,16 @@
+﻿
+
+using FinTrack.API.Core.Entities;
+using MediatR;
+
+namespace FinTrack.API.Application.UseCases.Accounts.GetAccount
+{
+
+    /// <summary>
+    /// Represents MediatR command for getting account by id.
+    /// Returns <see cref="Account"/> if exists
+    /// else returns <see langword="null"/>.
+    /// </summary>
+    /// <param name="guid">Account id</param>
+    public record GetAccountCommand(Guid guid) : IRequest<Account?>;
+}
---
+++ FinTrack/FinTrack.API.Application/UseCases/Accounts/GetAccount/GetAccountHandler.cs
@@ -0,0 +1,23 @@
+﻿
+
+using FinTrack.API.Core.Entities;
+using FinTrack.API.Core.Interfaces;
+using MediatR;
+
+namespace FinTrack.API.Application.UseCases.Accounts.GetAccount
+{
+    internal class GetAccountHandler : IRequestHandler<GetAccountCommand, Account?>
+    {
+        private readonly IAccountRepository _accountRepository;
+        public GetAccountHandler(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        async public Task<Account?> Handle(GetAccountCommand request, CancellationToken cancellationToken)
+        {
+            var account = await _accountRepository.GetByIdAsync(request.guid);
+            return account;
+        }
+    }
+}
