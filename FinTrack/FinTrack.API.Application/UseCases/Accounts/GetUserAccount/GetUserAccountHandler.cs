e1925ad971bcc379e828871772233cadddf72663
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-07-27 21:33:19 +0300

    feat(application): add use case for getting user account

---
+++ FinTrack/FinTrack.API.Application/UseCases/Accounts/GetUserAccount/GetUserAccountHandler.cs
@@ -0,0 +1,32 @@
+﻿
+
+using FinTrack.API.Core.Entities;
+using FinTrack.API.Core.Interfaces;
+using MediatR;
+
+namespace FinTrack.API.Application.UseCases.Accounts.GetUserAccount
+{
+    internal class GetUserAccountHandler : IRequestHandler<GetUserAccountCommand, Account?>
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public GetUserAccountHandler(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        async public Task<Account?> Handle(GetUserAccountCommand request, CancellationToken cancellationToken)
+        {
+            var account = await _accountRepository.GetByIdAsync(request.accountId);
+            if (account == null)
+            {
+                return null;
+            }
+            if (account.UserId != request.userId)
+            {
+                throw new ArgumentException($"account with id {request.accountId} does not belong to user {request.userId}");
+            }
+            return account;
+        }
+    }
+}
