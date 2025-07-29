81d8ef4f156e0dd272837aa6a3396d06ef5b408c
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-07-29 13:26:44 +0300

    feat(api): update AccountController

--- FinTrack/FinTrack.API/Controllers/AccountController.cs
+++ FinTrack/FinTrack.API/Controllers/AccountController.cs
@@ -1,14 +1,16 @@
 ﻿using FinTrack.API.Application.UseCases.Accounts.CreateAccount;
 using FinTrack.API.Application.UseCases.Accounts.DebitBalance;
 using FinTrack.API.Application.UseCases.Accounts.DeleteAccount;
-using FinTrack.API.Application.UseCases.Accounts.DeleteUserAccount;
 using FinTrack.API.Application.UseCases.Accounts.GetAccount;
-using FinTrack.API.Application.UseCases.Accounts.GetUserAccount;
 using FinTrack.API.Application.UseCases.Accounts.TopUpBalance;
+using FinTrack.API.Application.UseCases.Users.GetUser;
+using FinTrack.API.Core.Common;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.JsonWebTokens;
+using System.Data;
+using System.Security.Claims;
 
 namespace FinTrack.API.Controllers
 {
@@ -43,36 +45,15 @@ namespace FinTrack.API.Controllers
         [HttpGet("{guid}")]
         async public Task<IActionResult> GetAccountById(Guid guid)
         {
-            var command = new GetAccountCommand(guid);
-            var account = await _mediator.Send(command);
-            if (account == null)
-            {
-                return BadRequest(new { message = $"account with id {guid} does not exist" });
-            }
-            return Ok(new
-            {
-                id = guid,
-                balance = account.Balance,
-                user_id = account.UserId
-            });
-        }
-        [HttpGet("me/{guid}")]
-        async public Task<IActionResult> GetMyAccount(Guid guid)
-        {
-            var sub = User.FindFirst(JwtRegisteredClaimNames.Sub);
-            if (sub == null)
-            {
-                return BadRequest(new { message = "jwt token does not contains user id" });
-            }
-            var userGuid = Guid.Parse(sub.Value);
-            var command = new GetUserAccountCommand(userGuid, guid);
-            var account = await _mediator.Send(command);
+            var userGuid = GetCurrentUserGuid();
+            if (userGuid == null) return BadRequest(new { message = "jwt token does not contains user id" });
+
+            var roles = GetCurrentUserRoles();
+
+            var getAccountCommand = new GetAccountCommand(userGuid.Value, roles, guid);
+            var account = await _mediator.Send(getAccountCommand);
+            if (account == null) return NotFound(new { message = $"account with id {guid} does not exist" });
             
-            if ( account == null)
-            {
-                return BadRequest(new { message = $"account with id {guid} does not exist" });
-            }
-
             return Ok(new
             {
                 id = guid,
@@ -80,31 +61,24 @@ namespace FinTrack.API.Controllers
                 user_id = account.UserId
             });
         }
-
+       
 
 
         [HttpDelete("{guid}")]
         async public Task<IActionResult> DeleteAccount(Guid guid)
         {
-            var command = new DeleteAccountCommand(guid);
+            var userGuid = GetCurrentUserGuid();
+            if (userGuid == null) return BadRequest(new { message = "jwt token does not contains user id" });
+
+            var roles = GetCurrentUserRoles();
+
+            var command = new DeleteAccountCommand(userGuid.Value, roles, guid);
             await _mediator.Send(command);
             return NoContent();
         }
 
-        [HttpDelete("me/{guid}")]
-        async public Task<IActionResult> DeleteMyAccount(Guid guid)
-        {
-            var sub = User.FindFirst(JwtRegisteredClaimNames.Sub);
-            if (sub == null)
-            {
-                return BadRequest(new { message = "jwt token does not contains user id" });
-            }
-            var userId = Guid.Parse(sub.Value);
-            var command = new DeleteUserAccountCommand(userId, guid);
-            await _mediator.Send(command);
-            return NoContent();
-        }
 
+        [Authorize(Roles = UserRoles.Admin)]
         [HttpPost("{guid}/topup")]
         async public Task<IActionResult> TopUpAccountBalance(Guid guid, [FromQuery] int amount){
             var command = new TopUpBalanceCommand(guid, amount);
@@ -113,6 +87,7 @@ namespace FinTrack.API.Controllers
 
         }
 
+        [Authorize(Roles = UserRoles.Admin)]
         [HttpPost("{guid}/debit")]
         async public Task<IActionResult> DebitAccountBalance(Guid guid, [FromQuery] int amount)
         {
@@ -122,6 +97,19 @@ namespace FinTrack.API.Controllers
 
         }
 
-        
+        private Guid? GetCurrentUserGuid()
+        {
+            var claim = User.FindFirst(JwtRegisteredClaimNames.Sub);
+            if (claim == null) return null;
+            return Guid.Parse(claim.Value);
+        }
+
+        private List<string> GetCurrentUserRoles()
+        {
+            return User.Claims
+                .Where(c => c.Type == ClaimTypes.Role)
+                .Select(c => c.Value)
+                .ToList();
+        }
     }
 }
