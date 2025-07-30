91603e712173d3697391ba4e99a635c844bbdc83
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-07-30 14:10:24 +0300

    feat(application): add Result class

---
+++ FinTrack/FinTrack.API.Application/Common/Result.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace FinTrack.API.Application.Common
+{
+    public class Result : ResultBase
+    {
+
+        protected Result(bool isSuccess,
+                         string statusMessage) : base(isSuccess, statusMessage)
+        {
+
+        }
+        
+
+        public static Result Ok(string statusMessage) => new Result(true, statusMessage);
+        public static Result Fail(string statusMessage, Exception exception) => new Result(false, statusMessage);
+    }
+}
