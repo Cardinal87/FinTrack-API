580eb668acfc272f8511dafb9c27542ccbe38caf
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-07-30 14:08:52 +0300

    feat(application): add Result-pattern base class and statuses

---
+++ FinTrack/FinTrack.API.Application/Common/OperationStatusMessages.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace FinTrack.API.Application.Common
+{
+    public static class OperationStatusMessages
+    {
+        private const string Ok = "Ok";
+        private const string Created = "Created";
+        private const string NoContent = "NoContent";
+        private const string Forbidden = "Forbidden";
+        private const string NotFound = "NotFound";
+        private const string InternalError = "InternalError";
+        private const string BadRequest = "BadRequest";
+    }
+   
+}
---
+++ FinTrack/FinTrack.API.Application/Common/ResultBase.cs
@@ -0,0 +1,15 @@
+﻿
+namespace FinTrack.API.Application.Common
+{
+    public abstract class ResultBase
+    {
+        public bool IsSuccess { get; }
+        public string StatusMessage { get; }
+
+        protected ResultBase(bool isSuccess, string statusMessage)
+        {
+            IsSuccess = isSuccess;
+            StatusMessage = statusMessage;
+        }
+    }
+}
