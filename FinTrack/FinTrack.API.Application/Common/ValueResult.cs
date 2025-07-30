ccb25182889222eff0d42fb4038fba90e7a4e883
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-07-30 14:10:44 +0300

    feat(application): add ValueResult class

---
+++ FinTrack/FinTrack.API.Application/Common/ValueResult.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace FinTrack.API.Application.Common
+{
+    public class ValueResult<T> : ResultBase
+    {
+        public T? Value { get; }
+
+        private ValueResult(T? value,
+                            bool isSuccess,
+                            string statusMessage) : base(isSuccess, statusMessage)
+        {
+            Value = value;
+        }
+
+        public static ValueResult<T> Ok(T value, string statusMessage) => new ValueResult<T>(value, true, statusMessage);
+
+        public static ValueResult<T> Fail(string statusMessage, Exception exception) => new ValueResult<T>(default, false, statusMessage);
+    }
+}
