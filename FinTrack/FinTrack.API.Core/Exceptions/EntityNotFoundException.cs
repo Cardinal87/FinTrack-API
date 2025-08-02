af6fa77c52c43821381269fbcebce223111f5547
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-08-02 22:31:41 +0300

    docs(core): add exceptions documentation

--- FinTrack/FinTrack.API.Core/Exceptions/EntityNotFoundException.cs
+++ FinTrack/FinTrack.API.Core/Exceptions/EntityNotFoundException.cs
@@ -3,6 +3,13 @@ using FinTrack.API.Core.Common;
 
 namespace FinTrack.API.Core.Exceptions
 {
+    /// <summary>
+    /// Exception for showing that entity cannot be find
+    /// </summary>
+    /// <remarks>
+    /// Must be thrown when entity cannot be find by
+    /// id or by amother key
+    /// </remarks>
     public class EntityNotFoundException : DomainException
     {
         public EntityNotFoundException(string message) : base(message)
--- FinTrack/FinTrack.API.Core/Exceptions/IncorrectAmountException.cs
+++ FinTrack/FinTrack.API.Core/Exceptions/IncorrectAmountException.cs
@@ -3,10 +3,23 @@ using FinTrack.API.Core.Common;
 
 namespace FinTrack.API.Core.Exceptions
 {
+    /// <summary>
+    /// Exception for incorrect operation amount
+    /// </summary>
+    /// <remarks>
+    /// Must be thrown when amount <= 0
+    /// </remarks>
     public class IncorrectAmountException : DomainException
     {
+        /// <summary>
+        /// Invalid amount
+        /// </summary>
         public decimal Amount { get; }
         
+        /// <summary>
+        /// </summary>
+        /// <param name="amount">invalid amount</param>
+        /// <param name="message">exception message</param>
         public IncorrectAmountException(decimal amount, string message) : base(message)
         {
             Amount = amount;
