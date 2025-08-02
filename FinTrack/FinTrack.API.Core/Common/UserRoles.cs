913e1b0592861ca584c7449a7b6147e1bdd41f32
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-08-02 22:32:05 +0300

    docs(core): add UserRoles documentation

--- FinTrack/FinTrack.API.Core/Common/UserRoles.cs
+++ FinTrack/FinTrack.API.Core/Common/UserRoles.cs
@@ -2,11 +2,17 @@
 
 namespace FinTrack.API.Core.Common
 {
+    /// <summary>
+    /// Static class with user role constants
+    /// </summary>
     public static class UserRoles
     {
         public const string User = "User";
         public const string Admin = "Admin";
 
+        /// <summary>
+        /// Read-only collection with all possible user roles
+        /// </summary>
         public static IReadOnlyCollection<string> AllRoles = [
             User,
             Admin
