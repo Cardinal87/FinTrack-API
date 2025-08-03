ad125ba39cca411c79784ceaf5d1000b46f5df99
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-08-03 23:32:15 +0300

    feat(application): add use case for getting transaction by id

---
+++ FinTrack/FinTrack.API.Application/UseCases/Transactions/GetTransactionById/GetTransactionByIdCommand.cs
@@ -0,0 +1,19 @@
+﻿
+
+using FinTrack.API.Application.Common;
+using FinTrack.API.Core.Entities;
+using MediatR;
+
+
+namespace FinTrack.API.Application.UseCases.Transactions.GetTransactionById
+{
+    /// <summary>
+    /// Represents MediatR command for getting transaction by id.
+    /// Returns <see cref="ValueResult{T}"/> with <see cref="Transaction"/> if it exists and
+    /// user has permission to get it
+    /// else returns <see langword="null"/>.
+    /// </summary>
+    /// <param name="transactionId">Transaction id</param>
+    /// <param name="accountId">Id of the </param>
+    public record GetTransactionByIdCommand(Guid userId, IReadOnlyCollection<string> roles, Guid transactionId) : IRequest<ValueResult<Transaction>>;
+}
---
+++ FinTrack/FinTrack.API.Application/UseCases/Transactions/GetTransactionById/GetTransactionByIdHandler.cs
@@ -0,0 +1,44 @@
+﻿
+using FinTrack.API.Application.Common;
+using FinTrack.API.Core.Common;
+using FinTrack.API.Core.Entities;
+using FinTrack.API.Core.Interfaces;
+using MediatR;
+
+namespace FinTrack.API.Application.UseCases.Transactions.GetTransactionById
+{
+    internal class GetTransactionByIdHandler : IRequestHandler<GetTransactionByIdCommand, ValueResult<Transaction>>
+    {
+        private readonly ITransactionRepository _transactionRepository;
+        private readonly IAccountRepository _accountRepository;
+
+        public GetTransactionByIdHandler(ITransactionRepository transactionRepository, IAccountRepository accountRepository)
+        {
+            _transactionRepository = transactionRepository;
+            _accountRepository = accountRepository;
+        }
+
+        async public Task<ValueResult<Transaction>> Handle(GetTransactionByIdCommand request, CancellationToken cancellationToken)
+        {
+            var transaction = await _transactionRepository.GetByIdAsync(request.transactionId);
+            if (transaction == null)
+            {
+                return ValueResult<Transaction>.Fail(OperationStatusMessages.NotFound);
+            }
+            if (request.roles.Contains(UserRoles.Admin))
+            {
+                return ValueResult<Transaction>.Ok(transaction, OperationStatusMessages.Ok);
+            }
+
+            var fromAccount = await _accountRepository.GetByIdAsync(transaction.FromAccountId);
+            var toAccount = await _accountRepository.GetByIdAsync(transaction.ToAccountId);
+
+            if (fromAccount != null && toAccount != null && 
+                (toAccount.UserId == request.userId || fromAccount.UserId == request.userId))
+            {
+                return ValueResult<Transaction>.Ok(transaction, OperationStatusMessages.Ok);
+            }
+            return ValueResult<Transaction>.Fail(OperationStatusMessages.Forbidden);
+        }
+    }
+}
