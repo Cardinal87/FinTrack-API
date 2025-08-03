f9eb0472a518a86f1538c03479bfcaebcfe8bddf
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-08-03 23:31:52 +0300

    feat(application): add use case for getting account transactions

---
+++ FinTrack/FinTrack.API.Application/UseCases/Transactions/GetAccountTransactions/GetAccountTransactionsCommand.cs
@@ -0,0 +1,19 @@
+﻿
+using FinTrack.API.Application.Common;
+using FinTrack.API.Core.Entities;
+using MediatR;
+
+namespace FinTrack.API.Application.UseCases.Transactions.GetAccountTransactions
+{
+    /// <summary>
+    /// Represents MediatR command for getting all account transactions.
+    /// Returns <see cref="ValueResult{T}"/> constains <see cref="IReadOnlyCollection{T}"/> 
+    /// with <see cref="Transaction"/>s
+    /// </summary>
+    /// <param name="accountId">Account id</param>
+    /// <param name="userId">Id of the user that invokes command</param>
+    /// <param name="roles">User roles</param>
+    public record GetAccountTransactionsCommand(Guid userId,
+                                                IReadOnlyCollection<string> roles,
+                                                Guid accountId) : IRequest<ValueResult<IReadOnlyCollection<Transaction>>>;
+}
---
+++ FinTrack/FinTrack.API.Application/UseCases/Transactions/GetAccountTransactions/GetAccountTransactionsHandler.cs
@@ -0,0 +1,38 @@
+﻿
+using FinTrack.API.Application.Common;
+using FinTrack.API.Core.Interfaces;
+using FinTrack.API.Core.Common;
+using MediatR;
+using FinTrack.API.Core.Entities;
+
+namespace FinTrack.API.Application.UseCases.Transactions.GetAccountTransactions
+{
+    internal class GetAccountTransactionsHandler
+        : IRequestHandler<GetAccountTransactionsCommand, ValueResult<IReadOnlyCollection<Transaction>>>
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public GetAccountTransactionsHandler(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+        
+        
+        async public Task<ValueResult<IReadOnlyCollection<Transaction>>> Handle(GetAccountTransactionsCommand request, CancellationToken cancellationToken)
+        {
+            var account = await _accountRepository.GetByIdAsync(request.accountId);
+            if (account == null)
+            {
+                return ValueResult<IReadOnlyCollection<Transaction>>.Fail(OperationStatusMessages.NotFound);
+            }
+
+            if (request.roles.Contains(UserRoles.Admin)
+                || account.UserId == request.userId)
+            {
+                var combined = account.IncomingTransactions.Concat(account.OutgoingTransactions).ToList();
+                return ValueResult<IReadOnlyCollection<Transaction>>.Ok(combined.AsReadOnly(), OperationStatusMessages.Ok);
+            }
+            return ValueResult<IReadOnlyCollection<Transaction>>.Fail(OperationStatusMessages.Forbidden);
+        }
+    }
+}
