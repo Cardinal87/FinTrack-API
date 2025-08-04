0c0231d07fcc9ab80d6c663f82a1d5d66103122a
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-08-04 16:37:18 +0300

    feat(application): add use case to get transactions by date

---
+++ FinTrack/FinTrack.API.Application/UseCases/Transactions/GetTransactionsByDate/GetTransactionsByDateCommand.cs
@@ -0,0 +1,19 @@
+﻿
+using FinTrack.API.Application.Common;
+using FinTrack.API.Core.Entities;
+using MediatR;
+
+namespace FinTrack.API.Application.UseCases.Transactions.GetTransactionsByDate
+{
+
+    /// <summary>
+    /// Represents MediatR command for getting all transactions by specified date.
+    /// Returns <see cref="ValueResult{T}"/> constains <see cref="IReadOnlyCollection{T}"/> 
+    /// with <see cref="Transaction"/>s
+    /// </summary>
+    /// <param name="userId">User id</param>
+    /// <param name="roles">User roles</param>
+    /// <param name="date">Date of transaction</param>
+    public record GetTransactionsByDateCommand(Guid userId, IReadOnlyCollection<string> roles, DateOnly date) : IRequest<ValueResult<IReadOnlyCollection<Transaction>>>;
+   
+}
---
+++ FinTrack/FinTrack.API.Application/UseCases/Transactions/GetTransactionsByDate/GetTransactionsByDateHandler.cs
@@ -0,0 +1,43 @@
+﻿
+using FinTrack.API.Application.Common;
+using FinTrack.API.Core.Common;
+using FinTrack.API.Core.Entities;
+using FinTrack.API.Core.Interfaces;
+using MediatR;
+
+namespace FinTrack.API.Application.UseCases.Transactions.GetTransactionsByDate
+{
+    class GetTransactionsByDateHandler 
+        : IRequestHandler<GetTransactionsByDateCommand, ValueResult<IReadOnlyCollection<Transaction>>>
+    {
+        private readonly ITransactionRepository _transactionRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly IAccountRepository _accountRepository;
+
+        public GetTransactionsByDateHandler(ITransactionRepository transactionRepository,
+                                            IUserRepository userRepository,
+                                            IAccountRepository accountRepository)
+        {
+            _transactionRepository = transactionRepository;
+            _userRepository = userRepository;
+            _accountRepository = accountRepository;
+        }
+
+        async public Task<ValueResult<IReadOnlyCollection<Transaction>>> Handle(GetTransactionsByDateCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByIdAsync(request.userId);
+            if (user == null) return ValueResult<IReadOnlyCollection<Transaction>>.Fail(OperationStatusMessages.NotFound);
+
+
+            if (request.roles.Contains(UserRoles.Admin))
+            {
+                var transactions = await _transactionRepository.GetByDateAsync(request.date);
+                return ValueResult<IReadOnlyCollection<Transaction>>.Ok(transactions.ToList().AsReadOnly(), OperationStatusMessages.Ok);
+            }
+            var userAccoutsIds = await _accountRepository.GetAccountIdsByUserIdAsync(user.Id);
+
+            var allowed = await _transactionRepository.GetByDateAsync(request.date, userAccoutsIds);
+            return ValueResult<IReadOnlyCollection<Transaction>>.Ok(allowed.ToList().AsReadOnly(), OperationStatusMessages.Ok);
+        }
+    }
+}
