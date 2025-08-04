8344cfd9a3ffb76121b75943d072d665fba81d13
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-08-04 17:03:29 +0300

    feat(application): add use case to get transactions by time interval

---
+++ FinTrack/FinTrack.API.Application/UseCases/Transactions/GetTransactionsByTimeInterval/GetTransactionsByTimeIntervalCommand.cs
@@ -0,0 +1,23 @@
+﻿
+using FinTrack.API.Application.Common;
+using FinTrack.API.Core.Entities;
+using MediatR;
+
+namespace FinTrack.API.Application.UseCases.Transactions.GetTransactionsByTimeInterval
+{
+
+    /// <summary>
+    /// Represents MediatR command to get transactions by time interval.
+    /// Returns <see cref="ValueResult{T}"/> constains <see cref="IReadOnlyCollection{T}"/> 
+    /// with <see cref="Transaction"/>s
+    /// </summary>
+    /// <param name="userId">User id</param>
+    /// <param name="roles">User roles</param>
+    /// <param name="from">start of the interval</param>
+    /// <param name="to">end of the interval</param>
+    public record GetTransactionsByTimeIntervalCommand(Guid userId,
+                                                       IEnumerable<string> roles,
+                                                       DateTime from,
+                                                       DateTime to) : IRequest<ValueResult<IReadOnlyCollection<Transaction>>>;
+    
+}
---
+++ FinTrack/FinTrack.API.Application/UseCases/Transactions/GetTransactionsByTimeInterval/GetTransactionsByTimeIntervalHandler.cs
@@ -0,0 +1,44 @@
+﻿
+using FinTrack.API.Application.Common;
+using FinTrack.API.Core.Common;
+using FinTrack.API.Core.Entities;
+using FinTrack.API.Core.Interfaces;
+using MediatR;
+
+namespace FinTrack.API.Application.UseCases.Transactions.GetTransactionsByTimeInterval
+{
+    class GetTransactionsByTimeIntervalHandler
+        : IRequestHandler<GetTransactionsByTimeIntervalCommand, ValueResult<IReadOnlyCollection<Transaction>>>
+    {
+
+        private readonly ITransactionRepository _transactionRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly IAccountRepository _accountRepository;
+
+        public GetTransactionsByTimeIntervalHandler(ITransactionRepository transactionRepository,
+                                            IUserRepository userRepository,
+                                            IAccountRepository accountRepository)
+        {
+            _transactionRepository = transactionRepository;
+            _userRepository = userRepository;
+            _accountRepository = accountRepository;
+        }
+
+        async public Task<ValueResult<IReadOnlyCollection<Transaction>>> Handle(GetTransactionsByTimeIntervalCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByIdAsync(request.userId);
+            if (user == null) return ValueResult<IReadOnlyCollection<Transaction>>.Fail(OperationStatusMessages.NotFound);
+
+
+            if (request.roles.Contains(UserRoles.Admin))
+            {
+                var transactions = await _transactionRepository.GetFromToDateAsync(request.from, request.to);
+                return ValueResult<IReadOnlyCollection<Transaction>>.Ok(transactions.ToList().AsReadOnly(), OperationStatusMessages.Ok);
+            }
+            var userAccoutsIds = await _accountRepository.GetAccountIdsByUserIdAsync(user.Id);
+
+            var allowed = await _transactionRepository.GetFromToDateAsync(request.from, request.to, userAccoutsIds);
+            return ValueResult<IReadOnlyCollection<Transaction>>.Ok(allowed.ToList().AsReadOnly(), OperationStatusMessages.Ok);
+        }
+    }
+}
