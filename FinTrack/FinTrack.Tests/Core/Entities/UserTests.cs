ce3604ffa61d7608ba59ec86e96997f119820b22
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-08-09 20:38:25 +0300

    test(core): update UserTests

--- FinTrack/FinTrack.Tests/Core/Entities/UserTests.cs
+++ FinTrack/FinTrack.Tests/Core/Entities/UserTests.cs
@@ -26,6 +26,8 @@ namespace FinTrack.Tests.Core.Entities
             var invalidEmail6 = () => new User("user name@example.com", phone, name, hash);
             var invalidEmail7 = () => new User("user@example_com.com", phone, name, hash);
             var invalidEmail8 = () => new User("user@example.com.", phone, name, hash);
+            var invalidEmail9 = () => new User("", phone, name, hash);
+            var invalidEmail10 = () => new User(" ", phone, name, hash);
 
 
             validEmail.Should().NotThrow();
@@ -37,6 +39,8 @@ namespace FinTrack.Tests.Core.Entities
             invalidEmail6.Should().Throw<ArgumentException>();
             invalidEmail7.Should().Throw<ArgumentException>();
             invalidEmail8.Should().Throw<ArgumentException>();
+            invalidEmail9.Should().Throw<ArgumentException>();
+            invalidEmail10.Should().Throw<ArgumentException>();
         }
 
 
@@ -52,11 +56,13 @@ namespace FinTrack.Tests.Core.Entities
             var invalidPhone1 = () => new User(email, "+7", name, hash);
             var invalidPhone2 = () => new User(email, "+7912 345 6789", name, hash);
             var invalidPhone3 = () => new User(email, "+7abc1234567", name, hash);
+            var invalidPhone4 = () => new User(email, "", name, hash);
 
             validPhone.Should().NotThrow();
             invalidPhone1.Should().Throw<ArgumentException>();
             invalidPhone2.Should().Throw<ArgumentException>();
             invalidPhone3.Should().Throw<ArgumentException>();
+            invalidPhone4.Should().Throw<ArgumentException>();
 
         }
 
@@ -69,12 +75,20 @@ namespace FinTrack.Tests.Core.Entities
             var phone = "+79998887766";
 
 
-            var validHash1 = () => new User(email, phone, name, "SHA256.50.Y0ea1poJCyWCd+yPum+ZQZov+ySJgVEGV8lEzNEUjpc=.XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg=");
-            var validHash2 = () => new User(email, phone, name, "MD5.50.zrIHcuDJ0kDHXrJrDjer7g==.X03MO1qnZdYdgyfeuILPmQ==");
+            var validHash1 = () => new User(email,
+                                            phone,
+                                            name,
+                                            "SHA256.50.Y0ea1poJCyWCd+yPum+ZQZov+ySJgVEGV8lEzNEUjpc=.XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg=");
+            var validHash2 = () => new User(email,
+                                            phone,
+                                            name,
+                                            "MD5.50.zrIHcuDJ0kDHXrJrDjer7g==.X03MO1qnZdYdgyfeuILPmQ==");
 
-            var invalidHash1 = () => new User(email, phone, name, "10a6e6cc8311a3e2bcc09bf6c199adecd5dd59408c343e926b129c4914f3cb0");
-            var invalidHash2 = () => new User(email, phone, name, "10a6e6cc8311a3e2bcc09bf6c199adecd5dd59408c343e926b129c4914f3cb011");
-            var invalidHash3 = () => new User(email, phone, name, "");
+            var invalidHash1 = () => new User(email, phone, name, "X03MO1qnZdYdgyfeuILPmQ==");
+            var invalidHash2 = () => new User(email, phone, name, "zrIHcuDJ0kDHXrJrDjer7g==.X03MO1qnZdYdgyfeuILPmQ==");
+            var invalidHash3 = () => new User(email, phone, name, ".50.zrIHcuDJ0kDHXrJrDjer7g==.X03MO1qnZdYdgyfeuILPmQ==");
+            var invalidHash4 = () => new User(email, phone, name, "MD5$.50.zrIHcuDJ0kDHXrJrDjer7#==.X03MO1qnZdYdgyfeuILPm@==");
+            var invalidHash5 = () => new User(email, phone, name, "");
 
             validHash1.Should().NotThrow();
             validHash2.Should().NotThrow();
@@ -82,6 +96,8 @@ namespace FinTrack.Tests.Core.Entities
             invalidHash1.Should().Throw<ArgumentException>();
             invalidHash2.Should().Throw<ArgumentException>();
             invalidHash3.Should().Throw<ArgumentException>();
+            invalidHash4.Should().Throw<ArgumentException>();
+            invalidHash5.Should().Throw<ArgumentException>();
         }
 
         [Fact]
@@ -93,10 +109,12 @@ namespace FinTrack.Tests.Core.Entities
 
             var validName = () => new User(email, phone, "username", hash);
 
-            var invalidName = () => new User(email, phone, "", hash);
+            var invalidName1 = () => new User(email, phone, "", hash);
+            var invalidName2 = () => new User(email, phone, " ", hash);
 
             validName.Should().NotThrow();
-            invalidName.Should().Throw<ArgumentException>();
+            invalidName1.Should().Throw<ArgumentException>();
+            invalidName2.Should().Throw<ArgumentException>();
 
         }
 
@@ -116,11 +134,9 @@ namespace FinTrack.Tests.Core.Entities
 
             add_valid_account.Should().NotThrow();
             add_invalid_account.Should().Throw<AccountOwnershipException>();
+
             user.Accounts.Should().HaveCount(1);
             user.Accounts.Should().Contain(valid_account);
-
-
-
         }
 
         [Fact]
@@ -150,16 +166,18 @@ namespace FinTrack.Tests.Core.Entities
                                 "test_user",
                                 "SHA256.50.Y0ea1poJCyWCd+yPum+ZQZov+ySJgVEGV8lEzNEUjpc=.XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg=");
 
-            var addValidRole1 = () => user.AssignRole(UserRoles.Admin);
+            var addValidRole1 = () => user.AssignRole(UserRoles.User);
             var addValidRole2 = () => user.AssignRole(UserRoles.Admin);
 
             var addInvalidRole1 = () => user.AssignRole("IncorrectRole");
             var addInvalidRole2 = () => user.AssignRole("");
+            var addInvalidRole3 = () => user.AssignRole(" ");
 
             addValidRole1.Should().NotThrow();
             addValidRole2.Should().NotThrow();
             addInvalidRole1.Should().Throw<DomainException>();
             addInvalidRole2.Should().Throw<DomainException>();
+            addInvalidRole3.Should().Throw<DomainException>();
 
         }
     }
