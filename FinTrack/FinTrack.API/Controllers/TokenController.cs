a934c525c06e50736eaca3a9256b1d29e312a5a5
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-08-12 10:54:28 +0300

    docs(api): add TokenController documentation

--- FinTrack/FinTrack.API/Controllers/TokenController.cs
+++ FinTrack/FinTrack.API/Controllers/TokenController.cs
@@ -22,7 +22,30 @@ namespace FinTrack.API.Controllers
             _jwtTokenService = jwtTokenService;
         }
 
+        /// <summary>
+        /// Creates access JWT-token for user
+        /// </summary>
+        /// <param name="loginRequest">user credentials</param>
+        /// <remarks>
+        /// Request example:
+        /// POST /api/auth/token
+        /// {
+        ///     "login": "mylogin",
+        ///     "password": "mypassword"
+        /// }
+        /// 
+        /// Response example:
+        /// {
+        ///     "token": YOUR_ACCESS_TOKEN
+        /// }
+        /// </remarks>
+        /// <responce code="200">token created</responce>
+        /// <responce code="401">provided credentials are invalid or user does not exists</responce>
         [HttpPost("token")]
+        [Produces("application/json")]
+        [Consumes("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         async public Task<IActionResult> GetJwtToken([FromBody] LoginRequest loginRequest)
         {
             var request = new AuthUserCommand(loginRequest.Login, loginRequest.Password);
@@ -35,9 +58,21 @@ namespace FinTrack.API.Controllers
             return HandleFailedResult(result);
         }
 
-
+        /// <summary>
+        /// Returns status of provided JWT-token
+        /// </summary>
+        /// <remarks>
+        /// Request example:
+        /// GET /api/auth/token/status
+        /// -H "Authorization: Bearer YOUR_ACCESS_TOKEN"
+        /// </remarks>
+        /// <responce code="200">token is valid</responce>
+        /// <responce code="401">token invalid or does not provided</responce>
         [Authorize]
         [HttpGet("token/status")]
+        [Consumes("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public IActionResult GetJwtStatus()
         {
             return Ok(new { status = "token is valid" });
