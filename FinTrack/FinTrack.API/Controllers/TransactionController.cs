256a22826cb9d4fc59ec363ba5b347a8bfc7ea58
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-08-19 14:11:34 +0300

    docs(api): add TransactionController documentation

--- FinTrack/FinTrack.API/Controllers/TransactionController.cs
+++ FinTrack/FinTrack.API/Controllers/TransactionController.cs
@@ -1,5 +1,4 @@
-﻿using FinTrack.API.Application.Common;
-using FinTrack.API.Application.UseCases.Transactions.CreateTransaction;
+﻿using FinTrack.API.Application.UseCases.Transactions.CreateTransaction;
 using FinTrack.API.Application.UseCases.Transactions.GetAccountTransactions;
 using FinTrack.API.Application.UseCases.Transactions.GetTransactionById;
 using FinTrack.API.Application.UseCases.Transactions.GetTransactionsByDate;
@@ -9,8 +8,6 @@ using FinTrack.API.DTO;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
 
 namespace FinTrack.API.Controllers
 {
@@ -26,8 +23,32 @@ namespace FinTrack.API.Controllers
             _mediator = mediator;
         }
 
-
+        /// <summary>
+        /// Creates transaction between accounts
+        /// </summary>
+        /// <param name="request">data for creating transaction</param>
+        /// <remarks>
+        /// Request example:
+        /// POST /api/transactions
+        /// -H "Authorization: Bearer YOUR_ACCESS_TOKEN"
+        /// {
+        ///     "amount": "300",
+        ///     "source_account_id": "30dd879c-ee2f-11db-8314-0800200c9a66",
+        ///     "destination_account_id": "3b7c138f-fc68-42b8-a705-31417bb4cb56",
+        /// }
+        /// 
+        /// Response example:
+        /// {
+        ///     "id": "CREATED_Transaction_ID"
+        /// }
+        /// </remarks>
+        /// <responce code="201">transaction created successfully</responce>
+        /// <responce code="400">invalid request data</responce>
         [HttpPost]
+        [Produces("application/json")]
+        [Consumes("application/json")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         async public Task<IActionResult> CreateTransaction([FromBody] CreateTransactionRequest request)
         {
             var createTransactionCommand = new CreateTransactionCommand(request.Amount,
@@ -44,8 +65,34 @@ namespace FinTrack.API.Controllers
             return HandleFailedResult(result);
         }
 
-
+        /// <summary>
+        /// Returns transaction by id
+        /// </summary>
+        /// <param name="id">id of the existing transaction</param>
+        /// <remarks>
+        /// Request example:
+        /// GET /api/transactions/30dd879c-ee2f-11db-8314-0800200c9a66
+        /// -H "Authorization: Bearer YOUR_ACCESS_TOKEN"
+        /// 
+        /// Response example:
+        /// {
+        ///     "id": "30dd879c-ee2f-11db-8314-0800200c9a66",
+        ///     "amount": 300,
+        ///     "source_account_id": "85cb33aa-f7a5-4940-9bf2-7c50850925aa",
+        ///     "destination_account_id": "3b7c138f-fc68-42b8-a705-31417bb4cb56",
+        ///     
+        /// }
+        /// </remarks>
+        /// <response code="200">successfull request</response>
+        /// <response code="400">invalid request data</response>
+        /// <response code="403">user has not access to transaction</response>
+        /// <response code="404">transaction with <paramref name="id"/> not found</response>
         [HttpGet("{id}")]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         async public Task<IActionResult> GetTransactionById(Guid id)
         {
             
@@ -66,8 +113,35 @@ namespace FinTrack.API.Controllers
             return HandleFailedResult(result);
 
         }
-
+        /// <summary>
+        /// Returns transaction by date
+        /// </summary>
+        /// <param name="date">date of the transaction</param>
+        /// <remarks>
+        /// Request example:
+        /// GET /api/transactions/date/2025-08-19
+        /// -H "Authorization: Bearer YOUR_ACCESS_TOKEN"
+        /// 
+        /// Response example:
+        /// {
+        ///     [    
+        ///         {
+        ///             "id": "30dd879c-ee2f-11db-8314-0800200c9a66",
+        ///             "amount": 300,
+        ///             "source_account_id": "85cb33aa-f7a5-4940-9bf2-7c50850925aa",
+        ///             "destination_account_id": "3b7c138f-fc68-42b8-a705-31417bb4cb56",
+        ///         },
+        ///         ...
+        ///     ]
+        ///     
+        /// }
+        /// </remarks>
+        /// <response code="200">successfull request</response>
+        /// <response code="400">invalid request data</response>
         [HttpGet("date/{date}")]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         async public Task<IActionResult> GetTransactionsByDate(DateOnly date)
         {
 
@@ -84,7 +158,36 @@ namespace FinTrack.API.Controllers
             return HandleFailedResult(result);
         }
 
+        /// <summary>
+        /// Returns transaction by time interval
+        /// </summary>
+        /// <param name="start">start of the interval</param>
+        /// <param name="end">end of the interval</param>
+        /// <remarks>
+        /// Request example:
+        /// GET /api/transactions/interval?start=2025-08-10&amp;end=2025-08-13
+        /// -H "Authorization: Bearer YOUR_ACCESS_TOKEN"
+        /// 
+        /// Response example:
+        /// {
+        ///     [    
+        ///         {
+        ///             "id": "30dd879c-ee2f-11db-8314-0800200c9a66",
+        ///             "amount": 300,
+        ///             "source_account_id": "85cb33aa-f7a5-4940-9bf2-7c50850925aa",
+        ///             "destination_account_id": "3b7c138f-fc68-42b8-a705-31417bb4cb56",
+        ///         },
+        ///         ...
+        ///     ]
+        ///     
+        /// }
+        /// </remarks>
+        /// <response code="200">successfull request</response>
+        /// <response code="400">invalid request data</response>
         [HttpGet("interval")]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         async public Task<IActionResult> GetTransactionsByInterval([FromQuery] DateTime start,
                                                                    [FromQuery] DateTime end)
         {
@@ -104,7 +207,39 @@ namespace FinTrack.API.Controllers
             return HandleFailedResult(result);
         }
 
+        /// <summary>
+        /// Returns all transactions of specified account
+        /// </summary>
+        /// <param name="id">id of the existing account</param>
+        /// <remarks>
+        /// Request example:
+        /// GET /api/transactions/account/30dd879c-ee2f-11db-8314-0800200c9a66
+        /// -H "Authorization: Bearer YOUR_ACCESS_TOKEN"
+        /// 
+        /// Response example:
+        /// {
+        ///     [    
+        ///         {
+        ///             "id": "85cb33aa-f7a5-4940-9bf2-7c50850925aa",
+        ///             "amount": 300,
+        ///             "source_account_id": "30dd879c-ee2f-11db-8314-0800200c9a66",
+        ///             "destination_account_id": "3b7c138f-fc68-42b8-a705-31417bb4cb56",
+        ///         },
+        ///         ...
+        ///     ]
+        ///     
+        /// }
+        /// </remarks>
+        /// <response code="200">successfull request</response>
+        /// <response code="400">invalid request data</response>
+        /// <response code="403">user has not access to transactions of this account</response>
+        /// <response code="404">account with <paramref name="id"/> not found</response>
         [HttpGet("account/{id}")]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         async public Task<IActionResult> GetAccountTransactions(Guid id)
         {
 
