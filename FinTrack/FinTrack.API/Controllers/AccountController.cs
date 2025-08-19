86ea6e1e3836dd1d6d8476ef155e03a878e08ebc
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-08-19 13:41:20 +0300

    docs(api): add AccountController documentation

--- FinTrack/FinTrack.API/Controllers/AccountController.cs
+++ FinTrack/FinTrack.API/Controllers/AccountController.cs
@@ -1,19 +1,12 @@
-﻿using FinTrack.API.Application.Common;
-using FinTrack.API.Application.UseCases.Accounts.CreateAccount;
+﻿using FinTrack.API.Application.UseCases.Accounts.CreateAccount;
 using FinTrack.API.Application.UseCases.Accounts.DebitBalance;
 using FinTrack.API.Application.UseCases.Accounts.DeleteAccount;
 using FinTrack.API.Application.UseCases.Accounts.GetAccount;
 using FinTrack.API.Application.UseCases.Accounts.TopUpBalance;
-using FinTrack.API.Application.UseCases.Users.GetUser;
 using FinTrack.API.Controllers.Base;
-using FinTrack.API.Core.Common;
-using FinTrack.API.Core.Entities;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.JsonWebTokens;
-using System.Data;
-using System.Security.Claims;
 
 namespace FinTrack.API.Controllers
 {
@@ -30,7 +23,23 @@ namespace FinTrack.API.Controllers
             _mediator = mediator;
         }
 
+        /// <summary>
+        /// Creates account for user
+        /// </summary>
+        /// <remarks>
+        /// Request example:
+        /// POST /api/accounts
+        /// -H "Authorization: Bearer YOUR_ACCESS_TOKEN"
+        /// 
+        /// Response example:
+        /// {
+        ///     "id": "CREATED_ACCOUNT_ID"
+        /// }
+        /// </remarks>
+        /// <response code="201">account created successfully</response>
         [HttpPost()]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
         async public Task<IActionResult> CreateAccount()
         {
             var command = new CreateAccountCommand(UserId);
@@ -42,17 +51,43 @@ namespace FinTrack.API.Controllers
             return HandleFailedResult(result);
         }
 
-        [HttpGet("{guid}")]
-        async public Task<IActionResult> GetAccountById(Guid guid)
+        /// <summary>
+        /// Returns info abount account by id
+        /// </summary>
+        /// <param name="id"> id of the existing account</param>
+        /// <remarks>
+        /// Request example:
+        /// GET /api/accounts/30dd879c-ee2f-11db-8314-0800200c9a66
+        /// -H "Authorization: Bearer YOUR_ACCESS_TOKEN"
+        /// 
+        /// Response example:
+        /// {
+        ///     "id": "30dd879c-ee2f-11db-8314-0800200c9a66",
+        ///     "balance": 300,
+        ///     "user_id": "3b7c138f-fc68-42b8-a705-31417bb4cb56"
+        /// }
+        /// </remarks>
+        /// <response code="200">successfull request</response>
+        /// <response code="400">invalid request data</response>
+        /// <response code="403">user has not access to account</response>
+        /// <response code="404">account with <paramref name="id"/> not found</response>
+        [HttpGet("{id}")]
+        [Produces("application/json")]
+        [Consumes("application/json")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        async public Task<IActionResult> GetAccountById(Guid id)
         {
-            var getAccountCommand = new GetAccountCommand(UserId, UserRoles, guid);
+            var getAccountCommand = new GetAccountCommand(UserId, UserRoles, id);
             var result = await _mediator.Send(getAccountCommand);
             
             if (result.IsSuccess && result.Value != default)
             {
                 return Ok(new
                 {
-                    id = guid,
+                    id = id,
                     balance = result.Value.Balance,
                     user_id = result.Value.UserId
                 });
@@ -61,13 +96,30 @@ namespace FinTrack.API.Controllers
 
             
         }
-       
 
 
-        [HttpDelete("{guid}")]
-        async public Task<IActionResult> DeleteAccount(Guid guid)
+        /// <summary>
+        /// Deletes account by id
+        /// </summary>
+        /// <param name="id"> id of the existing account</param>
+        /// <remarks>
+        /// Request example:
+        /// DELETE /api/accounts/30dd879c-ee2f-11db-8314-0800200c9a66
+        /// -H "Authorization: Bearer YOUR_ACCESS_TOKEN"
+        /// 
+        /// </remarks>
+        /// <response code="204">account delete sucessfully</response>
+        /// <response code="400">invalid request data</response>
+        /// <response code="403">user has not access to account</response>
+        /// <response code="404">account with <paramref name="id"/> not found</response>
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        async public Task<IActionResult> DeleteAccount(Guid id)
         {
-            var command = new DeleteAccountCommand(UserId, UserRoles, guid);
+            var command = new DeleteAccountCommand(UserId, UserRoles, id);
             var result = await _mediator.Send(command);
             if (result.IsSuccess)
             {
@@ -76,11 +128,34 @@ namespace FinTrack.API.Controllers
             return HandleFailedResult(result);
         }
 
-
+        /// <summary>
+        /// Top ups account balance
+        /// </summary>
+        /// <param name="id"> id of the existing account</param>
+        /// <param name="amount">top up amount</param>
+        /// <remarks>
+        /// Request example:
+        /// POST /api/accounts/30dd879c-ee2f-11db-8314-0800200c9a66/topup?amount=300
+        /// -H "Authorization: Bearer YOUR_ACCESS_TOKEN"
+        /// 
+        /// Response example:
+        /// {
+        ///     "balance": 300,
+        /// }
+        /// </remarks>
+        /// <response code="204">account topped up successfully</response>
+        /// <response code="400">invalid request data</response>
+        /// <response code="403">user has not access to endpoint</response>
+        /// <response code="404">account with <paramref name="id"/> not found</response>
         [Authorize(Roles = Core.Common.UserRoles.Admin)]
-        [HttpPost("{guid}/topup")]
-        async public Task<IActionResult> TopUpAccountBalance(Guid guid, [FromQuery] int amount){
-            var command = new TopUpBalanceCommand(guid, amount);
+        [HttpPost("{id}/topup")]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        async public Task<IActionResult> TopUpAccountBalance(Guid id, [FromQuery] int amount){
+            var command = new TopUpBalanceCommand(id, amount);
             var result = await _mediator.Send(command);
             if (result.IsSuccess && result.Value != default)
             {
@@ -90,11 +165,36 @@ namespace FinTrack.API.Controllers
 
         }
 
+
+        /// <summary>
+        /// Debits account balance
+        /// </summary>
+        /// <param name="id"> id of the existing account</param>
+        /// <param name="amount">debit amount</param>
+        /// <remarks>
+        /// Request example:
+        /// POST /api/accounts/30dd879c-ee2f-11db-8314-0800200c9a66/debit?amount=300
+        /// -H "Authorization: Bearer YOUR_ACCESS_TOKEN"
+        /// 
+        /// Response example:
+        /// {
+        ///     "balance": 0,
+        /// }
+        /// </remarks>
+        /// <response code="204">account debited successfully</response>
+        /// <response code="400">invalid request data</response>
+        /// <response code="403">user has not access to endpoint</response>
+        /// <response code="404">account with <paramref name="id"/> not found</response>
         [Authorize(Roles = Core.Common.UserRoles.Admin)]
-        [HttpPost("{guid}/debit")]
-        async public Task<IActionResult> DebitAccountBalance(Guid guid, [FromQuery] int amount)
+        [HttpPost("{id}/debit")]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        async public Task<IActionResult> DebitAccountBalance(Guid id, [FromQuery] int amount)
         {
-            var command = new DebitBalanceCommand(guid, amount);
+            var command = new DebitBalanceCommand(id, amount);
             var result = await _mediator.Send(command);
             if (result.IsSuccess)
             {
